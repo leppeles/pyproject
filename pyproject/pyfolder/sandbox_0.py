f223dba9e2e6fa6168aec89b272481b571afa572
leppeles <19oszi91@gmail.com>
2018-10-07 10:23:46 +0200

    lin reg improved

--- pyproject/pyfolder/sandbox_0.py
+++ pyproject/pyfolder/sandbox_0.py
@@ -2,7 +2,9 @@ import torch
 import numpy as np
 from torch.nn import Linear
 
-w = torch.tensor(2.0,requires_grad = True)
+
+# linear equation without torch.nn
+w = torch.tensor(20.0,requires_grad = True)
 b = torch.tensor(-1.0,requires_grad = True)
 
 def forward(x):
@@ -11,8 +13,9 @@ def forward(x):
 
 x = torch.tensor([[1.0],[2.0]])
 yhat = forward(x)
-#print(yhat)
+print(yhat, "\n"*2)
 
+# linear equation with torch.nn's Linear
 torch.manual_seed(10)
 
 model = Linear(in_features=1,out_features=1)
