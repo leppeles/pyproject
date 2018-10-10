185d76bb25828ce1fb9b063ca7161cd53654387b
leppeles <19oszi91@gmail.com>
2018-10-10 21:34:02 +0200

    Gradient descents created

--- pyproject/pyfolder/sandbox_0.py
+++ pyproject/pyfolder/sandbox_0.py
@@ -1,9 +1,47 @@
 import torch
 import numpy as np
 from torch.nn import Linear
+import matplotlib.pyplot as plt
 
+X = torch.arange(-3,3,0.1).view(-1,1)
 
-# linear equation without torch.nn
+f = -3 * X
+
+Y= f + 1.1*torch.randn(X.size())
+
+plt.plot(X.numpy(),f.numpy())
+plt.plot(X.numpy(),Y.numpy(),'ro')
+
+w = torch.tensor(-10.0,requires_grad = True)
+
+def forward(x):
+    y = w*x
+    return y
+
+#calc R^2 tot the given model
+def criterion(yhat,y):
+    return torch.mean((yhat-y)**2)
+
+learningrate = 0.1
+
+#for epoch in range(5):
+LOSS = []
+loss = 100
+    
+while loss > 1.5:
+    Yhat = forward(X)
+    plt.plot(X.numpy(),Yhat.detach().numpy())
+    loss = criterion(Yhat,Y)
+    print(Yhat)
+    loss.backward()
+    w.data = w.data - learningrate * w.grad.data
+    w.grad.data.zero_()
+    LOSS.append(loss)
+    
+plt.show()
+
+'''linear equation without torch.nn
+
 w = torch.tensor(20.0,requires_grad = True)
 b = torch.tensor(-1.0,requires_grad = True)
 
@@ -24,3 +62,4 @@ print(list(model.parameters()))
 x=torch.tensor([[1.0],[2.0],[3.0],[4.0]])
 yhat = model(x)
 print(yhat)
+'''
