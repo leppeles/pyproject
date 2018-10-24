6552f8c4c41d051b103eb68316a59147c228b0b4
leppeles <19oszi91@gmail.com>
2018-10-24 18:24:46 +0200

    descent probes

--- pyproject/pyfolder/mbGradientDescent.py
+++ pyproject/pyfolder/mbGradientDescent.py
@@ -1,8 +1,69 @@
-
-import numpy as np
+import torch
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+from torch import nn,optim
+import plotter
 
 #mini-batch gradient descent
 
+torch.manual_seed(1)
+
+from torch.utils.data import Dataset, DataLoader
+class Data(Dataset):
+    def __init__(self):
+        self.x=torch.arange(-3,3,0.1).view(-1, 1)
+        self.f=1*self.x-1
+        self.y=self.f+1.1*torch.randn(self.x.size())
+        self.len=self.x.shape[0]
+    def __getitem__(self,index):    
+            
+        return self.x[index],self.y[index]
+    def __len__(self):
+        return self.len
+    
+dataset = Data()
+
+plt.plot(dataset.x.numpy(),dataset.y.numpy(),'rx',label='y')
+plt.plot(dataset.x.numpy(),dataset.f.numpy(),label='f')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+
+class linear_regression(nn.Module):
+    def __init__(self,input_size,output_size):
+        super(linear_regression,self).__init__()
+        self.linear=nn.Linear(input_size,output_size)
+    def forward(self,x):
+        yhat=self.linear(x)
+        return yhat
+    
+criterion = nn.MSELoss()
+
+model=linear_regression(1,1)
+optimizer = optim.SGD(model.parameters(), lr = 0.01)
+
+trainloader=DataLoader(dataset=dataset,batch_size=5)
+
+model.state_dict()['linear.weight'][0]=-15
+model.state_dict()['linear.bias'][0]=-10
+
+get_surface=plotter.plot_error_surfaces(15,13,dataset.x,dataset.y,30,go=False)
+
+epochs=5
+
+for epoch in range(epochs):
+    for x,y in trainloader:
+        #make a prediction 
+        yhat=model(x)
+        #calculate the loss
+        loss=criterion(yhat,y)
+        #for plotting 
+        get_surface.get_stuff(model,loss.tolist())
+        #clear gradient 
+        optimizer.zero_grad()
+        #Backward pass: compute gradient of the loss with respect to all the learnable parameters
+        loss.backward()
+        #the step function on an Optimizer makes an update to its parameters
+        optimizer.step()
+
+    get_surface.plot_ps()
 
--- pyproject/pyfolder/stochGradientDescent.py
+++ pyproject/pyfolder/stochGradientDescent.py
@@ -3,80 +3,9 @@
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
+import plotter
 
 from mpl_toolkits import mplot3d
-
-class plot_error_surfaces(object):
-    def __init__(self,w_range, b_range,X,Y,n_samples=30,go=True):
-        W = np.linspace(-w_range, w_range, n_samples)
-        B = np.linspace(-b_range, b_range, n_samples)
-        w, b = np.meshgrid(W, B)    
-        Z=np.zeros((30,30))
-        count1=0
-        self.y=Y.numpy()
-        self.x=X.numpy()
-        for w1,b1 in zip(w,b):
-            count2=0
-            for w2,b2 in zip(w1,b1):
-                Z[count1,count2]=np.mean((self.y-w2*self.x+b2)**2)
-                count2 +=1
-    
-            count1 +=1
-        self.Z=Z
-        self.w=w
-        self.b=b
-        self.W=[]
-        self.B=[]
-        self.LOSS=[]
-        self.n=0
-        if go==True:
-            plt.figure()
-            plt.figure(figsize=(7.5,5))
-            plt.axes(projection='3d').plot_surface(self.w, self.b, self.Z, rstride=1, cstride=1,cmap='viridis', edgecolor='none')
-            plt.title('Loss Surface')
-            plt.xlabel('w')
-            plt.ylabel('b')
-            plt.show()
-            plt.figure()
-            plt.title('Loss Surface Contour')
-            plt.xlabel('w')
-            plt.ylabel('b')
-            plt.contour(self.w, self.b, self.Z)
-            plt.show()
-    def get_stuff(self,W,B,loss):
-        self.n=self.n+1
-        self.W.append(W)
-        self.B.append(B)
-        self.LOSS.append(loss)
-        
-    def final_plot(self): 
-        ax = plt.axes(projection='3d')
-        ax.plot_wireframe(self.w, self.b, self.Z)
-        ax.scatter(self.W,self.B, self.LOSS, c='r', marker='x',s=200,alpha=1)
-        plt.figure()
-        plt.contour(self.w,self.b, self.Z)
-        plt.scatter(self.W,self.B,c='r', marker='x')
-        plt.xlabel('w')
-        plt.ylabel('b')
-        plt.show()
-    def plot_ps(self):
-        plt.subplot(121)
-        plt.ylim
-        plt.plot(self.x,self.y,'ro',label="training points")
-        plt.plot(self.x,self.W[-1]*self.x+self.B[-1],label="estimated line")
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.ylim((-10, 15))
-        plt.title('Data Space Iteration: '+str(self.n))
-        plt.legend()
-        plt.show()
-        plt.subplot(122)
-        plt.contour(self.w,self.b, self.Z)
-        plt.scatter(self.W,self.B,c='r', marker='x')
-        plt.title('Loss Surface Contour Iteration'+str(self.n) )
-        plt.xlabel('w')
-        plt.ylabel('b')
-        plt.legend()
 
 torch.manual_seed(1)
 
@@ -98,7 +27,7 @@ def forward(x):
 def criterion(yhat,y):
     return torch.mean((yhat-y)**2)
 
-get_surface=plot_error_surfaces(15,13,X,Y,30)
+get_surface=plotter.plot_error_surfaces(15,13,X,Y,30)
 
 epochs=10
 LOSS1=[]
